874ba637517599fab1d66cde878bfb767511c0fb
Steven King Jr <stevekx86@gmail.com>
2019-02-16 18:28:37 -0700

    Add a Typescript webpack loader for glsl files and TSLint

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(glsl|vs|fs)$/,
+        use: ['ts-shader-loader']
       }
     ]
   },
